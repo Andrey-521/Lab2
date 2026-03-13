5902587c075af897fde72ffcf08706f884194683
Andrey-521 <Lutcyk.andry@gmail.com>
2026-03-13 15:52:50 +0700

    Update OddEvenSeparator.h

--- Labar2/OddEvenSeparator.h
+++ Labar2/OddEvenSeparator.h
@@ -4,13 +4,13 @@ using namespace std;
 #include <vector>
 
 class OddEvenSeparator {
+    std::vector<int> numbers;
 public:
     OddEvenSeparator();
     void add_number(int x);
     void even() const; // печатает чётные числа в порядке добавления
     void odd() const;  // печатает нечётные числа в порядке добавления
-private:
-    std::vector<int> numbers;
+
 };
 
 #endif // ODDEVENSEPARATOR_H
